18a7a39f612041367533e896be5f1e1880a7248c
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-03-04 12:03:06 -0800

    Comply with JSON:API
    - Mime types
    - Validate content_type header
    - Set default url in dev & test environments
    - Use :json_api adapter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  before_action :check_header
+
+private
+  def check_header
+    if ['POST','PUT','PATCH'].include? request.method
+      if request.content_type != "application/vnd.api+json"
+        head 406 and return
+      end
+    end
+  end
+
+  def validate_type
+    if params['data'] && params['data']['type']
+      if params['data']['type'] == params[:controller]
+        return true
+      end
+    end
+    head 409 and return
+  end
+  
+  def render_error(resource, status)
+    render json: resource, status: status, adapter: :json_api,
+           serializer: ActiveModel::Serializer::ErrorSerializer
+  end
 end
