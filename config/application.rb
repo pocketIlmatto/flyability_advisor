f7866d759b22827710c3f0f41f0237c3fea7b10e
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-03-11 22:29:41 -0700

    Rename references to skycrackcast to flyability_advisor

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,12 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SkycrackcastApi
+module FlyabilityAdvisor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.autoload_paths << "#{Rails.root}/lib"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -29,5 +30,6 @@ module SkycrackcastApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
   end
 end
