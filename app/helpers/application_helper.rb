61a161c162cde9ea7e912dfb3a6e167de5fab1a9
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-03-14 10:51:15 -0700

    Show temp in NWS graph on fly_site page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def nws_plotter_url(fly_site)
+  def nws_plotter_url(fly_site, with_temp = false)
+    pcmd = with_temp ? '10001110000000000000000000000000000000000000000000000000000' : '00001110000000000000000000000000000000000000000000000000000'
     url = "https://forecast.weather.gov/meteograms/Plotter.php?" +
       "lat=#{fly_site.lat}&lon=#{fly_site.lng}&wfo=#{fly_site.nws_meta_data[:properties][:cwa]}&" +
       "zcode=#{fly_site.nws_meta_data[:forecastZone]}&gset=18&gdiff=3&unit=0&tinfo=PY8" +
-      "&ahour=0&pcmd=00001110000000000000000000000000000000000000000000000000000" +
-      "&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6"
+      "&ahour=0&pcmd=#{pcmd}&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6"
   end
 end
