2539c0b6a96b61b5fcd6ca4c8ca62c254903ebcc
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-12 16:08:06 -0700

    Create User model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#index'
   resources :fly_sites, only: [:create, :edit, :get_fly_sites, :new, :show, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_082655) do
+ActiveRecord::Schema.define(version: 2019_04_12_230647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -52,6 +52,18 @@ ActiveRecord::Schema.define(version: 2019_03_14_082655) do
     t.datetime "updated_at", null: false
     t.index ["fly_site_id"], name: "index_forecasts_on_fly_site_id"
     t.index ["source", "fly_site_id", "data_updated_at"], name: "index_forecasts_on_source_and_fly_site_id_and_data_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
