5e01a64230b627229772fc6231b586daf29dc903
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-12 16:40:46 -0700

    Add omniauth fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_230647) do
+ActiveRecord::Schema.define(version: 2019_04_12_234013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -62,6 +62,10 @@ ActiveRecord::Schema.define(version: 2019_04_12_230647) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
