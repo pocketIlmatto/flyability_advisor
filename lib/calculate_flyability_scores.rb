707c649ed86789d2be5ae454d249e1529d381aa6
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-13 12:38:06 -0700

    Update flyability calculation to 1.0.1
    - Takes into account precipitation forecast
    - Persists the details of the scores

--- lib/calculate_flyability_scores.rb
+++ lib/calculate_flyability_scores.rb
@@ -1,11 +1,14 @@
 class CalculateFlyabilityScores
 
-  CALCULATION_VERSION = '1.0.0'
+  # 1.0.1 Uses the short forecast to determine precipitation levels
+  CALCULATION_VERSION = '1.0.1'
 
   def self.calculate(fly_site)
     ideal_count = 0
     maybe_count = 0
     scores = Hash.new(0)
+    score_details = {}
+    short_forecast_no_gos = ["Rain Showers", "Chance Rain Showers"]
 
     todaynum = Time.now.wday
 
@@ -21,15 +24,9 @@ class CalculateFlyabilityScores
 
     for i in 0..155
       period_data = nws_data["periods"][i]
-      time_str = period_data["startTime"]
-      time_hour = time_str[11,3].strip.to_i
-      is_utc = time_str[20,2]
-      if is_utc == '00'
-        time_hour = time_hour - 7 # TODO don't hard-code this timezone fix
-        if (time_hour < 0)
-          time_hour = time_hour + 24
-        end
-      end
+      
+      time_hour = Time.parse(period_data["startTime"]).hour
+      score_details[todaynum] = {} if i == 0
 
       wind_speed = period_data["windSpeed"]
       speed_min_act = 0
@@ -45,9 +42,10 @@ class CalculateFlyabilityScores
       speed_max_act = speed_max_act.try(:strip).try(:to_i)
 
       wind_direction = period_data["windDirection"]
+      short_forecast = period_data["shortForecast"]
 
+      # Starting new day, so total up the scores
       if time_hour == 0
-        # Starting new day, so total up the scores
         if ideal_count >= 3
           scores[todaynum] = "hell-yes"
         elsif ideal_count >= 1
@@ -59,26 +57,46 @@ class CalculateFlyabilityScores
         end
         
         todaynum += 1
+        score_details[todaynum] = {} 
         
         ideal_count = 0
         maybe_count = 0
-      elsif time_hour >= fly_site.hourstart && time_hour <= fly_site.hourend
+      end
+
+      score_details[todaynum][time_hour] = {
+        short_forecast: short_forecast,
+        speed_min_act: speed_min_act,
+        speed_max_act: speed_max_act,
+        wind_direction: wind_direction,
+        wind_speed: wind_speed
+      }
+      
+      if time_hour >= fly_site.hourstart && time_hour <= fly_site.hourend
         if speed_min_act >= fly_site.speedmin_ideal &&
           speed_max_act <= fly_site.speedmax_ideal &&
-          fly_site.dir_ideal.include?(wind_direction)
+          fly_site.dir_ideal.include?(wind_direction) &&
+          !short_forecast_no_gos.include?(short_forecast)
+          score_details[todaynum][time_hour][:score] = 'ideal'
           ideal_count += 1
           maybe_count += 1
         elsif speed_min_act >= fly_site.speedmin_edge && 
           speed_max_act <= fly_site.speedmax_edge &&
           fly_site.dir_edge.include?(wind_direction)
+          !short_forecast_no_gos.include?(short_forecast)
+          score_details[todaynum][time_hour][:score] = 'edge'
           maybe_count += 1
+        else
+          score_details[todaynum][time_hour][:score] = 'no'
         end
+      else
+        score_details[todaynum][time_hour][:score] = 'not_in_flying_window'
       end
     end
     
     ::FlyabilityScore.create(calculation_version: CALCULATION_VERSION, 
-      fly_site_id: fly_site.id, scores: scores)
+      fly_site_id: fly_site.id, scores: scores, score_details: score_details)
     Rails.logger.info "Flyability scores calculated for #{fly_site.slug}"
   end
 
 end
+
