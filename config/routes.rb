48cbc8d79a0e9b1382774a4460f94b9ce8348479
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-12 17:20:28 -0700

    Configure omniauth to work with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :fly_sites, only: [:create, :edit, :get_fly_sites, :new, :show, :update]
 end
