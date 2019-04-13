60c0bc071eb530023f0020195335dee7049d8790
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-12 18:10:47 -0700

    Update routing to send missing route hits to 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, path_names: {
+    sign_up: '/404.html',
+    sign_in: '/404.html'
+  }
   resources :fly_sites, only: [:create, :edit, :get_fly_sites, :new, :show, :update]
+  get '*path' => redirect('/404.html')
 end
