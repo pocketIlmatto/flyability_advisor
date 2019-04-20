869a72404cd55dc73de43d69e2732fb9826936fa
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-19 23:45:33 -0400

    Make minor improvement to HomeHelper syntax

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
   DOW_MAP = {"0" => "Su", "1" => "M", "2" => "Tu", "3" => "W", "4" => "Th", "5" => "F", "6" => "Sa"}
 
   def get_dow(wkday)
-    wkday = (wkday.to_i - 7).to_s if wkday.to_i >= 7
+    wkday = wkday.to_i % 7
     DOW_MAP[wkday.to_s]
   end
 
