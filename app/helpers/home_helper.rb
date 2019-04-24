cbb68499d91c39d10a953da8eb73be1820e76f3e
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-23 23:05:02 -0700

    Make improvements to the expanded score forecast
    - Always align under the gradient by creating separate rows
    - Use directional arrows for wind direction
    - Remove icons
    - Add an 'x' to close the expanded section

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -19,15 +19,53 @@ module HomeHelper
     end
   end
 
+  def get_directional_arrow_class(direction)
+    case direction
+    when "SSE"
+      "rotate-337.5"
+    when "SE"
+      "rotate-315"
+    when "ESE"
+      "rotate-292.5"
+    when "E"
+      "rotate-270"
+    when "ENE"
+      "rotate-247.5"
+    when "NE"
+      "rotate-225"
+    when "NNE"
+      "rotate-202.5"
+    when "N"
+      "rotate-180"
+    when "NNW"
+      "rotate-157.5"
+    when "NW"
+      "rotate-135"
+    when "WNW"
+      "rotate-112.5"
+    when "W"
+      "rotate-90"
+    when "WSW"
+      "rotate-67.5"
+    when "SW"
+      "rotate-45"
+    when "SSW"
+      "rotate-22.5"
+    when "S"
+      ""
+    else
+      "spin"
+    end
+  end
+
   def translate_score_details_for_display(score_details_dow)
-    display_details = {scores: [], hours: [], speeds: [], directions: [], icons: []}
+    display_details = {scores: [], hours: [], speeds: [], directions: []}
     score_details_dow.each_pair do |hour, score_details_dow_hour|
       next if score_details_dow_hour["score"] == "not_in_flying_window"
       display_details[:hours] << hour
       display_details[:scores] << score_details_dow_hour["score"]
       display_details[:speeds] << score_details_dow_hour["speed_max_act"]
-      display_details[:directions] << score_details_dow_hour["wind_direction"]
-      display_details[:icons] << score_details_dow_hour["icon"]
+      display_details[:directions] << get_directional_arrow_class(score_details_dow_hour["wind_direction"])
     end
     display_details
   end
