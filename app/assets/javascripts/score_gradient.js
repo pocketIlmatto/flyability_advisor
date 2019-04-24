1d63a4888c83210b66f72c6a4cafd4c5e92c228e
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-23 20:31:38 -0700

    Improve the forecast gradient

--- app/assets/javascripts/score_gradient.js
+++ app/assets/javascripts/score_gradient.js
@@ -5,15 +5,15 @@ var ScoreGradient = {
     if (c) {
       var ctx = c.getContext("2d");
 
-      var grd = ctx.createLinearGradient(0, 0, c.width, c.height);
-
+      var grd = ctx.createLinearGradient(0, 0, c.width, 0);
+      
       periods = this.countPeriods(scores);
       currentPeriod = 0;
       for (var i in scores) {
         var key = i;
         var score = scores[i].score;
         if (score != "not_in_flying_window") {
-          grd.addColorStop(currentPeriod/periods, this.determineColor(score));
+          grd.addColorStop(currentPeriod/periods, this.determineColor(score, scores[i].speed_max_act));
         }
         currentPeriod += 1
       }
@@ -27,25 +27,61 @@ var ScoreGradient = {
     }
   },
 
-  determineColor: function(score) {
+  determineColor: function(score, windSpeed) {
     var color;
+    var colorModifier = (10 - windSpeed)/100;
     switch(score) {
       case "ideal":
-        color = "green"
+        color = this.changeColorByX("#008000", colorModifier, 0, colorModifier);
         break;
       case "edge":
-        color = "darkcyan"
+        color = this.changeColorByX("#FFA500", 0, 0, colorModifier);
         break;
       case "no":
-        color = "blue"
+        color = this.changeColorByX("#A9A9A9", colorModifier, colorModifier, colorModifier);
         break;
       case "not_in_flying_window":
-        color = "white"
+        color = this.changeColorByX("#F5F5F5", colorModifier, colorModifier, colorModifier);
         break;
       default:
-        color = "white"
+        color = "#F5F5F5"
     }
     return color;
+  },
+
+  componentToHex: function(c) {
+    var hex = c.toString(16);
+    
+    hex = hex.length == 1 ? "0" + hex : hex;
+    return hex;
+  },
+
+  hexToRgb: function(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+  },
+
+  rgbToHex: function(r, g, b) {
+    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+  },
+
+  changeColorCache: {},
+
+  changeColorByX(baseColor, r, g, b) {
+    if (`${baseColor}${r}${g}${b}` in this.changeColorCache) {
+      return this.changeColorCache[`${baseColor}${r}${g}${b}`];
+    }
+    rgb = this.hexToRgb(baseColor);
+    rgb.r = parseInt((rgb.r * r) + rgb.r);
+    rgb.g = parseInt((rgb.g * g) + rgb.g);
+    rgb.b = parseInt((rgb.b * b) + rgb.b);
+    hex = this.rgbToHex((rgb.r >= 255 ? 255 : rgb.r), (rgb.g >= 255 ? 255 : rgb.g), (rgb.b >= 255 ? 255 : rgb.b));
+    this.changeColorCache[`${baseColor}${r}${g}${b}`] = hex;
+    return hex;
   },
 
   countPeriods: function(object) {
