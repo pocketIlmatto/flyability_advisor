c949b257b72d8243f3cef94d4bdf48cbe2e478d2
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-04-30 19:20:53 -0700

    Add time-related scopes to hourly tables

--- app/models/hourly_flyability_score.rb
+++ app/models/hourly_flyability_score.rb
@@ -1,2 +1,13 @@
 class HourlyFlyabilityScore < ApplicationRecord
+  scope :current, -> do 
+    where("start_time >= ?", Time.now.utc.change(min: 0))
+    .order(start_time: :asc)
+  end
+  
+  scope :by_days_ahead, -> num_days, time_zone do
+    where("start_time >= ? AND start_time <= ?", 
+      (Time.now.in_time_zone(time_zone).beginning_of_day.utc + num_days.days), 
+      ((Time.now.in_time_zone(time_zone).end_of_day.utc + num_days.days)))
+    .order(start_time: :asc)
+  end
 end
