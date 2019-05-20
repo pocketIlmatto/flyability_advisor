9d241514b7b5a219bd03dfd8b0d18107f100c272
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-05-20 14:07:44 -0700

    Change column highighlighting behavior
    - Fades other columns

--- app/assets/javascripts/dow_row_highlighter.js
+++ app/assets/javascripts/dow_row_highlighter.js
@@ -4,40 +4,53 @@ var DOWRowHighlighter = {
       if ($(this).hasClass("dow-highlight-border") == false) {
         // Remove the left border from the column to the right
         $(`.${i+1}-date-header`).removeClass("dow-regular-header-border");
-        $(`.${i+1}-date`).each(function() {
-          $(this).removeClass("dow-regular-border");
-        });
-
-        // Reset everywhere else
+        
+        // Reset borders everywhere else
         for (var j = 0; j < 7; j++) {
           // Remove the highlight from anything else
           if (j != i) {
             $(`.${j}-date-header`).removeClass("dow-highlight-border");
-            $(`.${j}-date`).each(function() {
-              $(this).removeClass("dow-highlight-border");
-            });
           }
           // Add the regular header border, and regular border to anything other than i+1
           if (j != i + 1) {
             $(`.${j}-date-header`).addClass("dow-regular-header-border");
+          }
+        }
+
+        // Show the selected column
+        $(this).fadeTo("fast", 1);
+        $(`.${i}-date`).each(function() {
+          $(this).fadeTo("fast", 1);
+        });
+
+        // Hide everything else
+        for (var j = 0; j < 7; j++) {
+          // Remove the highlight from anything else
+          if (j != i) {
+            $(`.${j}-date-header`).fadeTo("fast", 0.1);
             $(`.${j}-date`).each(function() {
-              $(this).addClass("dow-regular-border");
+              $(this).fadeTo("fast", 0.1);
             });
           }
         }
       } else {
         // Add the left border from the column to the right
         $(`.${i+1}-date-header`).addClass("dow-regular-header-border");
-        $(`.${i+1}-date`).each(function() {
-          $(this).addClass("dow-regular-border");
-        });
+
+        // Show everything else
+        for (var j = 0; j < 7; j++) {
+          // Remove the highlight from anything else
+          if (j != i) {
+            $(`.${j}-date-header`).fadeTo("fast", 1);
+            $(`.${j}-date`).each(function() {
+              $(this).fadeTo("fast", 1);
+            });
+          }
+        }
       }
 
       // Toggle highlight for this one
       $(this).toggleClass("dow-highlight-border");
-      $(`.${i}-date`).each(function() {
-        $(this).toggleClass("dow-highlight-border");
-      });
       
       e.preventDefault();
     });
